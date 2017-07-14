077a118e059d1a72d6f7fe72048bbf2f9169d2a4
cesar-vargas88 <vargas.cesar88@gmail.com>
2017-07-14 00:56:20 -0500

    Line detection try.

--- main.cpp
+++ main.cpp
@@ -9,42 +9,58 @@
 using namespace std;
 using namespace cv;
 
-Mat SegmentAgave(Mat imagen, list <int> &lsegmentPixels);
-Mat GroupAgave(Mat imagen, list <int> &lsegmentPixels, list <list <int> > &lgroupPixels);
-
-
-RNG rng(12345);
-
-
+Mat SegmentAgave(Mat imagen);
+Mat GroupAgave(Mat imagen);
 
 int main(int argc, char *argv[])
 {
-    Mat imgIn   = imread("/home/cesar/AgaveDetection/Images/Agave2.jpg", CV_LOAD_IMAGE_COLOR);
+    // Open the image
+    Mat imgIn   = imread("/home/cesar/AgaveDetection/Images/CRT3_Arenal_V2 100%.jpg", CV_LOAD_IMAGE_COLOR);
+    Mat imgOut = imgIn.clone();
 
     list <int>          lSegmentPixels;
     list <list <int> >  lGroupPixels;
 
+    // Verify that image opened correctly
     if(imgIn.empty())
     {
        cout << "Could not open image." << endl;
        return -1;
     }
 
-    namedWindow("Input Image", CV_WINDOW_AUTOSIZE );
-    imshow("Input Image", imgIn);
+    //GaussianBlur( imgIn, imgOut, Size( 9, 9 ), 0, 0 );
 
-    namedWindow("Agave Segmentation", CV_WINDOW_AUTOSIZE );
-    imshow("Agave Segmentation", SegmentAgave(imgIn, lSegmentPixels));
+    // Reduce noise with a kernel 3x3
+    //blur( src_gray, detected_edges, Size(3,3) );
 
-    namedWindow("Group Agave", CV_WINDOW_AUTOSIZE );
-    imshow("Group Agave", GroupAgave(imgIn, lSegmentPixels, lGroupPixels));
+    // Canny detector
+    //Canny( detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, kernel_size );
+
+    // Using Canny's output as a mask, we display our result
+    //dst = Scalar::all(0);
+
+
+
+    Mat gray, edge, draw;
+
+    //cvtColor(imgIn, gray, CV_BGR2GRAY);ŝŝ
+
+    //Canny(imgOut, edge, 50, 150, 3);
+    //edge.convertTo(draw, CV_8U);
+    // Create the image GaussianBlur
+
+    // Create the image AgeveSegmentation.jpg
+    imwrite("/home/cesar/AgaveDetection/Images/ProcessedImages/AgaveSegmentation.jpg", SegmentAgave(imgIn));
+
+    // Create the image AgeveGroup.jpg
+    //imwrite("/home/cesar/AgaveDetection/Images/ProcessedImages/AgaveGroup.jpg", GroupAgave(SegmentAgave(imgIn)));
 
     waitKey(0);
 
     return 0;
 }
 
-Mat SegmentAgave(Mat imgIn, list <int> &lsegmentPixels)
+Mat SegmentAgave(Mat imgIn)
 {
     Mat imgOut = imgIn.clone();
     Vec3b   vColor;
@@ -56,18 +72,11 @@ Mat SegmentAgave(Mat imgIn, list <int> &lsegmentPixels)
             // Get pixel
             vColor = imgOut.at<Vec3b>(Point(x, y));
 
-            if((vColor[0] - vColor[2]) > 34)         // Difference between blue and red is mayor than 40
+            if(((vColor[0] - vColor[2]) > 25) && ((vColor[0] - vColor[1]) > 0))         // Difference between blue and red is mayor than 38
             {
                 vColor[0] = 255;
                 vColor[1] = 255;
                 vColor[2] = 255;
-                                        // ( ActualRow *  TotalCols  ) + ActualCol
-                lsegmentPixels.push_front( (     y     * imgOut.cols ) +     x    );
-
-                cout << "Num Pixel: " << lsegmentPixels.size()
-                     << "\tSave Pixel: " << lsegmentPixels.front()
-                     << "\tRow: " << lsegmentPixels.front()/imgOut.cols
-                     << "\tCol: " << lsegmentPixels.front()%imgOut.cols << endl;
             }
             else
             {
@@ -81,138 +90,60 @@ Mat SegmentAgave(Mat imgIn, list <int> &lsegmentPixels)
         }
     }
 
+    GaussianBlur( imgOut, imgIn, Size( 3, 3 ), 0, 0 );
+
     cout << "Rows: " << imgOut.rows << "\tCols: " << imgOut.cols << "\tTotal Pixels: " <<  imgOut.rows * imgOut.cols << endl;
 
     return imgOut;
 }
 
 
-
-Mat GroupAgave(Mat imagen, list <int> &lsegmentPixels, list <list <int> > &lgroupPixels)
+Mat GroupAgave(Mat imgIn)
 {
-   /* Mat imgOut = imagen.clone();
-    Vec3b   vColor;
-
+    Mat canny_output;
     vector<vector<Point> > contours;
     vector<Vec4i> hierarchy;
 
-    findContours( imagen, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0) );
 
-    Mat drawing = Mat::zeros( imagen.size(), CV_8UC3 );
+    int thresh = 100;
+    RNG rng(12345);
 
-    for( size_t i = 0; i< contours.size(); i++ )
+    // Detect edges using canny
+    Canny( imgIn, canny_output, thresh, thresh*2, 3 );
+
+    // Find contours
+    findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+
+    vector<Moments> mu(contours.size());
+    vector<Point2f> mc( contours.size());
+    Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
+    int nCountContours = 1;
+
+    cout << "\n\nThere are a total of " << contours.size() << " contours." << endl;
+    cout << "\nInfo: Area and Contour Length" << endl;
+
+    /// Calculate the area with the moments 00 and compare with the result of the OpenCV function
+    for( int i = 0; i< contours.size(); i++ )
     {
-       Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-       drawContours( drawing, contours, (int)i, color, 2, 8, hierarchy, 0, Point() );
-    }
-*/
-    return imagen;
-}
+        // Get the moments
+        mu[i] = moments( contours[i], false );
 
-/*
+        // Get the mass centers
+        mc[i] = Point2f( mu[i].m10/mu[i].m00 , mu[i].m01/mu[i].m00 );
 
-Mat GroupAgave(Mat imagen, list <int> &lsegmentPixels, list <list <int> > &lgroupPixels)
-{
-    Mat imgOut = imagen.clone();
-    //Vec3b   vColor;
+        // Draw contours
+        Scalar color = Scalar( 0, 0, 255 );
+        //drawContours( drawing, contours, i, color, 1, 8, hierarchy, 0, Point() );
 
-    int nPixelRow           = 0;
-    int nPixelCol           = 0;
-    int nActualPixel        = 0;
-    int nNeighborPixel      = 0;
-    int nListPixelPosition  = 1;
-    bool bMorePixelInGroup  = true;
-    list <int> lPixelsInGroup;
-    list <int> lsegmentPixelsCopy;
-
-    while(lsegmentPixels.size())
-    {
-        cout << "Position: " << lsegmentPixels.back() << endl;
-
-        lPixelsInGroup.push_front(lsegmentPixels.back());
-
-        bMorePixelInGroup  = true;
-
-        while(bMorePixelInGroup)
+        if(contourArea(contours[i]) > -10)
         {
-            nActualPixel    = lsegmentPixels.back();
-            nPixelRow       = lsegmentPixels.back() / imgOut.cols;
-            nPixelCol       = lsegmentPixels.back() % imgOut.cols;
+            // Draw circles
+            color = Scalar( 0, 255, 0 );
+            circle( drawing, mc[i], 1, color, 1, 4, 0 );
 
-            cout << "nActualPixel: " << nActualPixel << endl;
-            cout << "nPixelRow: " << nPixelRow << endl;
-            cout << "nPixelCol: " << nPixelCol << endl;
-            cout << "x-1: " << ( nPixelRow * imgOut.cols ) + nPixelCol-1 << endl;
-            cout << "x+1: " << ( nPixelRow * imgOut.cols ) + nPixelCol+1 << endl;
-            cout << "y-1: " << ( (nPixelRow-1) * imgOut.cols ) + nPixelCol << endl;
-            cout << "y+1: " << ( (nPixelRow+1) * imgOut.cols ) + nPixelCol << endl;
-
-            // Add nActualPixel to the lPixelsInGroup list
-            lPixelsInGroup.push_back(nActualPixel);
-
-            // Remove last element of lsegmentPixels list
-            lsegmentPixels.pop_back();
-
-            if(nPixelCol-1 >= 0)
-            {
-                // Assign pixel number to nNeighborPixel
-                nNeighborPixel = ( nPixelRow * imgOut.cols ) + nPixelCol-1;
-
-                cout << "nPixelCol-1 >= 0 " << nNeighborPixel << endl;
-
-                // Make a copy of lsegmentPixels list
-                lsegmentPixelsCopy  = lsegmentPixels;
-                nListPixelPosition  = 1;
-
-                cout << "lPixelsInGroupCopy Size: " << lPixelsInGroupCopy.Size << endl;
-
-                // Verify if nNeighborPixel is in lsegmentPixelsCopy
-                while(lsegmentPixelsCopy.size())
-                {
-                    if(lsegmentPixelsCopy.back() == nNeighborPixel)
-                    {
-                        // Add nNeighborPixel to the lPixelsInGroup list
-                        lPixelsInGroup.push_back(nNeighborPixel);
-
-                        // Erase the element of nNeighborPixel from the lsegmentPixels list
-                        lsegmentPixels.erase(nListPixelPosition);
-
-                        // Clear lsegmentPixelsCopy list
-                        lsegmentPixelsCopy.clear();
-                    }
-                    else
-                    {
-                        // Remocoutve last element of copy of lsegmentPixels list
-                        lsegmentPixelsCopy.pop_back();
-                        nListPixelPosition++;
-                    }
-
-                    // Remove last element of copy of lsegmentPixels list
-                    lsegmentPixels.pop_back();
-                }
-            }
-
-            if(nPixelCol+1 < imgOut.cols)
-            {
-                nNeighborPixel = ( nPixelRow * imgOut.cols ) + nPixelCol+1;
-                cout << "nPixelCol+1 < imgOut.cols " << nNeighborPixel << endl;
-            }
-            if(nPixelRow-1 >= 0)
-            {
-                nNeighborPixel = ( (nPixelRow-1) * imgOut.cols ) + nPixelCol;
-                cout << "nPixelRow-1 >= 0 " << nNeighborPixel << endl;
-            }
-            if(nPixelRow+1 < imgOut.rows)
-            {
-                nNeighborPixel = ( (nPixelRow+1) * imgOut.cols ) + nPixelCol;
-                cout << "nPixelRow+1 < imgOut.rows " << nNeighborPixel << endl;
-            }
+            cout << nCountContours++ << "-"<< i+1 << "\t  Area (M_00) = " << mu[i].m00 << "\t  Area OpenCV: " << contourArea(contours[i]) << "\t  Length: " << arcLength( contours[i], true ) << endl;
         }
-
-        //lgroupPixels.push_front(list <int> {10});
-
     }
 
-    return imgOut;
+    return drawing;
 }
-*/
